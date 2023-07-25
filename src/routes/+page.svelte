056640912d3a46d542941c7b4aaacdb9ce6164e3
deniandriancode <deniandriancode@gmail.com>
2023-07-25 17:57:06 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,35 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  let email = "";
+
+  function subscribe() {
+	email = "";
+  }
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Chief Rico</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<main
+  class="h-full bg-top bg-cover bg-no-repeat bg-[url('/header_bg1.jpg')]
+		 text-white"
+  >
 
-		to your new<br />SvelteKit app
-	</h1>
+  <section
+	class="h-full px-4 md:px-32
+		   flex flex-col justify-center items-center"
+	>
+	<h1 class="text-2xl md:text-4xl font-bold text-center py-4 my-4 px-4 border-4 border-white">CHIEF RICO</h1>
+	<h1 class="text-4xl md:text-6xl tracking-tight font-extrabold text-center">Our website is launching soon</h1>
+	<p class="text-l md:text-xl text-center py-4 font-thin opacity-75">We are working hard to make an awesome website with lot of intutive features. Subscribe below to get notified when we launch and get some huge discounts as a beta tester.</p>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	<form on:submit|preventDefault={subscribe}>
+	  <input class="bg-transparent focus:border-transparent focus:ring-current focus:outline-transparent
+					w-96"
+			 type="email" bind:value={email} placeholder="Enter you email"/>
+	  <button class="my-4 sm:my-0 py-3 sm:py-0 sm:h-full md:w-auto px-4 bg-indigo-800 hover:bg-indigo-700 focus:bg-indigo-700"
+			  type="submit">Notify Me</button>
+	</form>
+  </section>
+  
+</main>
